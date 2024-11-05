6ba442df2e762acf2c0016890bf54382b6ad2e07
AniBukhrashvili <92999532+AniBukhrashvili@users.noreply.github.com>
2024-11-05 16:19:03 +0400

    homework 12

--- homework-12/task-01/index.js
+++ homework-12/task-01/index.js
@@ -1,41 +1,15 @@
-const https = require("https");
-const fs = require("fs");
+const fs = require("fs").promises;
 
-function fetchData(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let data = "";
-
-      res.on("data", (chunk) => (data += chunk));
-
-      res.on("end", () => {
-        try {
-          resolve(JSON.parse(data));
-        } catch (error) {
-          reject("Error parsing JSON");
-        }
-      });
-    });
-  });
+async function main() {
+  try {
+    const dirs = await fs.readdir(__dirname);
+    for (const dir of dirs) {
+      const stats = await fs.lstat(dir);
+      console.log(stats.isDirectory());
+    }
+  } catch (e) {
+    console.error("Error: ", e);
+  }
 }
 
-fetchData("https://jsonplaceholder.typicode.com/users")
-  .then((users) => {
-    const filteredUsers = users.map((user) => ({
-      id: user.id,
-      name: user.name,
-      username: user.username,
-      email: user.email,
-    }));
-
-    fs.writeFile("users.json", JSON.stringify(filteredUsers), (err) => {
-      if (err) {
-        console.error("Error: ", err);
-      } else {
-        console.log("Data written to users.json");
-      }
-    });
-  })
-  .catch((err) => {
-    console.error("Error: ", err);
-  });
+main();
--- homework-12/task-02/index.js
+++ homework-12/task-02/index.js
@@ -1,28 +1,74 @@
-const fs = require("fs");
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
-const [, , carModel, carReleaseDate, carColor] = process.argv;
+const PORT = 3000;
+const DATA_FILE = path.join(__dirname, "data.json");
 
-const car = {
-  id: Date.now(),
-  carModel: carModel,
-  carColor: carColor,
-  carReleaseDate: carReleaseDate,
-};
+const randomHTML = `
+        <html>
+          <head>
+            <title>EXAMPLE</title>
+          </head>
+          <body>
+            <h1>ME</h1>
+          </body>
+        </html>`;
 
-fs.readFile("cars.json", "utf8", (err, data) => {
-  let cars = [];
+const randomData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+  },
+  {
+    id: 3,
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+  },
+  {
+    id: 4,
+    name: "Bob Brown",
+    email: "bob.brown@example.com",
+  },
+];
 
-  if (!err && data) {
-    cars = JSON.parse(data);
-  }
-
-  cars.push(car);
-
-  fs.writeFile("cars.json", JSON.stringify(cars), (err) => {
-    if (err) {
-      console.error("Error: ", err);
-    } else {
-      console.log("Car added to cars.json");
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      try {
+        await fs.access(DATA_FILE);
+        const data = await fs.readFile(DATA_FILE, "utf8");
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    } else if (req.url === "/random") {
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(randomNumber));
+    } else if (req.url === "/html") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(randomHTML);
+    } else if (req.url === "/current-time") {
+      const currentTime = new Date().toISOString();
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ currentTime }));
+    } else if (req.url === "/api") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(randomData));
     }
-  });
+  } else {
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
